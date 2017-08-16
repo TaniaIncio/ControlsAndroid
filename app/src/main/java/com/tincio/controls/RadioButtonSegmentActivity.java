50dead0758bab315e12ac9e61411d64d2e51e451
taniaincio <tincio.gomez@gmail.com>
2017-08-16 03:14:42 -0500

    add segment

---
+++ app/src/main/java/com/tincio/controls/RadioButtonSegmentActivity.java
@@ -0,0 +1,13 @@
+package com.tincio.controls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioButtonSegmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_button_segment);
+    }
+}
